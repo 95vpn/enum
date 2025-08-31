efac88d61c35a09df0fe4aa786d8a7681718e542
Johnny Herrera <jmjaramillo95@gmail.com>
2025-08-30 23:29:44 -0500

    Agregar archivos de proyecto.

---
+++ enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estaciones alergia = Estaciones.Primaveta;
+            Console.WriteLine(alergia);
+        }
+    }
+    enum Estaciones { Primaveta, Verano, Otoño, Invierno};
+}
