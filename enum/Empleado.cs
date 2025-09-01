7c256a325d31693d986b890abca6420ae51224c2
Johnny Herrera <jmjaramillo95@gmail.com>
2025-08-31 20:52:55 -0500

    enum

---
+++ enum/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  @enum
+{
+    public class Empleado
+    {
+        public Empleado(Bonus bonusEmpleado, double salario)
+        {
+            bonus = bonusEmpleado;
+
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return salario + (double)bonus;
+        }
+
+
+        private double salario;
+        private Bonus bonus;
+    }
+    public enum Bonus { bajo=500, normal=1000, bueno=1500, extra=3000}
+}
--- enum/Program.cs
+++ enum/Program.cs
@@ -6,13 +6,37 @@ using System.Threading.Tasks;
 
 namespace @enum
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            /*
             Estaciones alergia = Estaciones.Primaveta;
+
+            String La_alergia = alergia.ToString(); 
             Console.WriteLine(alergia);
+            */
+
+            /*
+            Bonus Antonio = Bonus.bueno;
+
+            double bonusAntonio = (double)Antonio;
+
+            double salarioAntonio = 1500 + bonusAntonio;
+
+            Console.WriteLine(salarioAntonio);
+            */
+
+            Empleado Juan = new Empleado(Bonus.extra, 1900.50);
+
+            Console.WriteLine("El salario de empleado es: " + Juan.getSalario());
+
         }
     }
+    /*
     enum Estaciones { Primaveta, Verano, Otoño, Invierno};
+    */
+
+    //enum Bonus { bajo=500, normal=1000, bueno=1500, extra=3000};
+
 }
